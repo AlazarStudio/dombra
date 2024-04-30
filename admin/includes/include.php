8efcd1e48be697489bf90969f563f8aa6ea024f1
Ameron666 <aisakebi69@gmail.com>
2024-04-30 15:20:12 +0300

    release version;

--- admin/includes/include.php
+++ admin/includes/include.php
@@ -2,7 +2,8 @@
 session_start();
 require 'rb/rb.php';
 
-R::setup( 'mysql:host=localhost; dbname=dombra_db', 'root', '');
+R::setup( 'mysql:host=localhost; dbname=alimdzmn_db', 'alimdzmn_db', 'lGWh35*Z');
+// R::setup( 'mysql:host=localhost; dbname=dombra_db', 'root', '');
 
 if (!R::testConnection()) {
     exit('Нет подключения к БД');
